56d611ce9cd8256128b2c11b04ae6b8a2db6a386
bunnymarie <bunnymarie@protonmail.com>
2018-06-01 15:09:56 -0700

    Fixes bugs and creates functioning but unstyled quiz.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
   //front end
-  $("#date").submit(function(event){
+  $("#quiz").submit(function(event){
     var one = parseInt($("input:radio[name=oneChoice]:checked").val());
     var two = parseInt($("input:radio[name=twoChoice]:checked").val());
     var three = parseInt($("input:radio[name=threeChoice]:checked").val());
     var four = parseInt($("input:radio[name=fourChoice]:checked").val());
     var five = parseInt($("input:radio[name=fiveChoice]:checked").val());
     var result = one + two + three + four + five;
-
-    if (match <= 25) {
+    debugger;
+    if (result <= 25) {
       $("#ones-showing").show()
-    } else if (match <= 50) {
+    } else if (result <= 50) {
       $("#twos-showing").show()
-    } else if (match <= 75) {
+    } else if (result <= 75) {
       $("#threes-showing").show()
     } else {
       $("#fours-showing").show()
